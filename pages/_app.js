5c949b235eb7ae5fccbcc81f9418aecd382d731a
androgitai <andras.kelemen84@gmail.com>
2022-05-15 21:54:28 +0100

    scaling meta tag added

--- pages/_app.js
+++ pages/_app.js
@@ -10,6 +10,10 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>byAndro</title>
         <meta
+          name='viewport'
+          content='initial-scale=1.0,minimum-scale=1.0, maximum-scale = 2.0, width=device-width'
+        />
+        <meta
           name='description'
           content='byAndro&copy;. My name is Andras `Andro` Kelemen and this is my portfolio. I am a web developer working with mainly JavaScript,React and Next Js'
         />
