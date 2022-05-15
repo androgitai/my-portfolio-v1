309ba22fb064f51ad2dd7c2151160d954a1253b3
androgitai <andras.kelemen84@gmail.com>
2022-05-15 21:54:16 +0100

    hero split for components

--- components/hero/hero.js
+++ components/hero/hero.js
@@ -5,12 +5,6 @@ import Orbit from './orbit';
 const Hero = () => {
   return (
     <section className={styles.hero}>
-      <div className={styles.heroWelcome}>
-        <h1 className={styles.welcomeTop}>Welcome to</h1>
-        <h1 className={styles.welcomeBot}>
-          <span>byAndro</span>
-        </h1>
-      </div>
       <div className={styles.heroTxt}>
         <h2>
           Hi! I'm <span>Andras Kelemen</span>
