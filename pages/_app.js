df504a3c394310505f209b86d85668c84615a562
androgitai <andras.kelemen84@gmail.com>
2022-05-16 23:22:26 +0100

    temporary script

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Script from 'next/script';
 import { Fragment } from 'react';
 import Layout from '../components/layout/layout';
 
@@ -9,6 +10,11 @@ function MyApp({ Component, pageProps }) {
     <Layout>
       <Head>
         <title>byAndro</title>
+        <script
+          rel='stylesheet'
+          src='https://kit.fontawesome.com/ba0aa7f5da.js'
+          crossorigin='anonymous'
+        ></script>
         <meta
           name='viewport'
           content='initial-scale=1.0,minimum-scale=1.0, maximum-scale = 2.0, width=device-width'
