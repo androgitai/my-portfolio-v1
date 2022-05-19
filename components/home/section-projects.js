b723404396c3e43865c9f5745d6d0a65586aa12f
androgitai <andras.kelemen84@gmail.com>
2022-05-19 22:05:51 +0100

    data moved

--- components/home/section-projects.js
+++ components/home/section-projects.js
@@ -1,31 +1,9 @@
 import { Fragment } from 'react';
+import Link from 'next/link';
 import styles from '../../styles/components/home/section-projects.module.scss';
 import SectionProjectItem from './section-project-item';
-
-const projectsData = [
-  {
-    id: 1,
-    title: 'InvoiceMe',
-    subTitle: 'Web application for invoicing.',
-    paragraph1:
-      'Self developed production app where you can manage your invoices with ease in a safe manner.',
-    paragraph2:
-      'Uses React, Redux, CSS modules and FireBase for backend database and authentication.',
-    image: '/projects/invoiceMe/MainPic.jpg',
-    reverse: false,
-  },
-  {
-    id: 2,
-    title: 'REST Countries',
-    subTitle: 'Find detials about any country around the world.',
-    paragraph1:
-      'Self developed production app where you can search countries with flags, open and see detials about them.',
-    paragraph2:
-      'Uses Html5, Sass and JavaScript. Built to MVC architecture and uses REST countries API.',
-    image: '/projects/rest-countries/rest-countries.jpg',
-    reverse: true,
-  },
-];
+import { projectsData } from '../../data/projects';
+import Button from '../ui/button';
 
 const SectionProjects = () => {
   return (
@@ -44,9 +22,15 @@ const SectionProjects = () => {
             Featured Projects <div className='spacer fluid accent'></div>
           </h2>
 
-          {projectsData.map(item => (
-            <SectionProjectItem key={item.id} project={item} />
-          ))}
+          {projectsData.map(item => {
+            if (item.id > 2) return;
+            return <SectionProjectItem key={item.id} project={item} style='accent' />;
+          })}
+          <div className={styles.control}>
+            <Link href='/projects'>
+              <Button btnStyle='accent'>See all Projects &#8811;</Button>
+            </Link>
+          </div>
         </section>
       </div>
     </Fragment>
