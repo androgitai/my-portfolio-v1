621ac6003f4e3fefdb979ebfbde344411bef1d2f
androgitai <andras.kelemen84@gmail.com>
2022-05-20 21:58:46 +0100

    small quote refactor

--- components/home/section-projects.js
+++ components/home/section-projects.js
@@ -1,34 +1,36 @@
 import { Fragment } from 'react';
 import Link from 'next/link';
-import styles from '../../styles/components/home/section-projects.module.scss';
+
 import SectionProjectItem from './section-project-item';
-import { projectsData } from '../../data/projects';
 import Button from '../ui/button';
+
+import styles from '../../styles/components/home/section-projects.module.scss';
+import { projectsData } from '../../data/projects';
 
 const SectionProjects = () => {
   return (
     <Fragment>
       <div className={styles.wrapper}>
         <section className={styles.featured}>
-          <div className='quote'>
-            <p>"Make it work</p>
-            <p>
-              <span className='accent'>Make it right </span>
-            </p>
-            <p>Make it fast"</p>
+          <div className={`quote`}>
+            <q>
+              Make it work, <span className='accent'>Make it right </span>, Make it fast
+            </q>
+            <small>~ Kent Beck</small>
             <div className='spacer center accent'></div>
           </div>
           <h2>
             Featured Projects <div className='spacer fluid accent'></div>
           </h2>
-
           {projectsData.map(item => {
             if (item.id > 2) return;
             return <SectionProjectItem key={item.id} project={item} style='accent' />;
           })}
           <div className={styles.control}>
             <Link href='/projects'>
-              <Button btnStyle='accent'>See all Projects &#8811;</Button>
+              <a>
+                <Button btnStyle='accent'>See all Projects &#8811;</Button>
+              </a>
             </Link>
           </div>
         </section>
