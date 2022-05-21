29ef9ac37d8e4a2113c4569b7e754b1d2d6c218d
androgitai <andras.kelemen84@gmail.com>
2022-05-21 23:25:33 +0100

    added animations

--- components/home/section-contact.js
+++ components/home/section-contact.js
@@ -1,10 +1,20 @@
 import { Fragment } from 'react';
+import { useInView } from 'react-intersection-observer';
+
 import styles from '../../styles/components/home/section-contact.module.scss';
 import ContactForm from '../form/contact-form';
 
 const SectionContact = () => {
+  const { ref: contactRef, inView: conatctInView } = useInView({
+    threshold: 0.4,
+    triggerOnce: true,
+  });
+
   return (
-    <section className={styles.contact}>
+    <section
+      className={conatctInView ? `${styles.contact} floatFromTop` : 'hidden'}
+      ref={contactRef}
+    >
       <div className='quote'>
         <p>
           "What you can do <span className='secondary'>today</span> ,
@@ -17,7 +27,9 @@ const SectionContact = () => {
       <h2>
         Contact Me<div className='spacer secondary'></div>
       </h2>
+
       <ContactForm />
+
       <h3>Let's start your next project today!</h3>
       <div className='spacer center secondary'></div>
       <div></div>
