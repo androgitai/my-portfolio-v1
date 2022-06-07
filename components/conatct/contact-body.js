f410dcb6f84f521095b75f5309d2e44d3a50669a
androgitai <andras.kelemen84@gmail.com>
2022-06-07 11:42:25 +0100

    minor update

--- components/conatct/contact-body.js
+++ components/conatct/contact-body.js
@@ -6,6 +6,10 @@ const ContactBody = () => {
   return (
     <div className={styles.wrapper}>
       <section className={styles.contactBody}>
+        <h3>
+          Contact Me to start your next project!
+          <div className='line secondary center'></div>
+        </h3>
         <ContactForm />
       </section>
       <div className='line secondary fit'></div>
